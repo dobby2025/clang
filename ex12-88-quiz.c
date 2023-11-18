dd93de09e3e070e3153301711c6237fa1bfe3ffc
ITSC <sca@koreaedugroup.com>
2023-11-18 11:13:54 +0900

    ex12-88

--- ex12-88-quiz.c
+++ ex12-88-quiz.c
@@ -30,3 +30,41 @@
 8 X 9 = 72      9 X 9 = 81
 
 */
+
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+    // 2 X 1 = 2       3 X 1 = 3       4 X 1 = 4
+/*
+
+
+*/
+
+    for(int k = 0; k < 3; k++) 
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                int dan = j + 2;
+                int multiply = i + 1;
+                int result = dan * multiply;
+
+                printf("%d X %d = %d\t", dan, multiply, result);
+
+            }
+
+            printf("\n");
+        }
+
+        printf("\n");
+
+    }
+    
+
+
+    return 0;
+}

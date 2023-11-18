f7bae2391ee0a8eb627d78001709173b2cf99784
ITSC <sca@koreaedugroup.com>
2023-11-19 08:46:14 +0900

    malloc

--- ex18-03-heap-malloc.c
+++ ex18-03-heap-malloc.c
@@ -23,7 +23,7 @@ int main(void)
     char *result = getString();
     printf("당신이 좋아하는 단어는: %s\n", result);
 
-    if(result != NULL) free(result)
+    if(result != NULL) free(result);
 
     return 0;
 }

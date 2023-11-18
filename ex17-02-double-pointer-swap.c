ed91130a58bfb6e2132517744696c17522e82215
ITSC <sca@koreaedugroup.com>
2023-11-18 11:52:06 +0900

    mptr

---
+++ ex17-02-double-pointer-swap.c
@@ -0,0 +1,35 @@
+/*
+파일명: ex17-02-double-pointer-swap.c
+*/
+
+#include <stdio.h>
+
+void swapNumber(int **dptr1, int **dptr2) 
+{
+    int *temp;
+
+    temp = *dptr1;
+    *dptr1 = *dptr2;
+    *dptr2 = temp;
+
+}
+
+int main(void)
+{
+    int num1 = 66, num2 = 99;
+    int *ptr1 = &num1;
+    int *ptr2 = &num2;
+
+    printf("num1: %d num2: %d\n", num1, num2);
+    printf("&ptr1: %p &ptr2: %p\n", &ptr1, &ptr2);
+    printf("ptr1: %p ptr2: %p\n", ptr1, ptr2);
+    printf("*ptr1: %d *ptr2: %d\n", *ptr1, *ptr2);
+
+    swapNumber(&ptr1, &ptr2);
+
+    printf("num1: %d num2: %d\n", num1, num2);
+    printf("ptr1: %p ptr2: %p\n", ptr1, ptr2);
+    printf("*ptr1: %d *ptr2: %d\n", *ptr1, *ptr2);
+
+    return 0;
+}

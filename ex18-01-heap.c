3523c1742a54630b9526cea9fd3acea9679436fd
ITSC <sca@koreaedugroup.com>
2023-11-19 08:45:18 +0900

    heap

--- ex18-01-heap.c
+++ ex18-01-heap.c
@@ -1,5 +1,10 @@
 /*
 파일명: ex18-01-heap.c
+
+힙 영역 필요성
+    예제 처럼 지역변수 사용시 함수를 벗어나면 소멸
+    전역변수 사용시 코드가 복잡해지고 관리가 힘들어 질 수 있다.
+
 */
 
 #include <stdio.h>
@@ -7,8 +12,8 @@
 char* getString(void)
 {
     char str[100];
-    printf("좋아하는 단어?");
-    gets(str);
+    printf("좋아하는 단어는?");
+    gets(str); //문자열 입력함수
 
     printf("함수 안에서 결과: %s\n", str);
 

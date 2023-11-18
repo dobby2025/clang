9aedf1702e84bb7221115cd43587ea402d80bfcf
ITSC <sca@koreaedugroup.com>
2023-11-18 11:24:38 +0900

    x-mas

--- ex12-25-christmas.c
+++ ex12-25-christmas.c
@@ -1,9 +1,36 @@
+/*
+
+파일명: ex12-25-christmas.c
+
+*/
+
 #include <stdio.h>
 
 int main(void) 
 {
 
-    
+    printf("   ☆\n");
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if(j < 3 - i)
+            {
+                printf(" ");
+            }
+            else if (j > 3 + i)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
 
+    }
+    printf("  | |");
+   
     return 0;
 }

1bca1f276090cd6b255738b32114ce74b5c0131b
ITSC <sca@koreaedugroup.com>
2023-11-19 10:11:48 +0900

    malloc

--- ex18-03-heap-malloc.c
+++ ex18-03-heap-malloc.c
@@ -6,13 +6,17 @@ malloc 함수를 사용하여 지역변수 문제 해결 예제!
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 char* getString(void)
 {
     char *str = (char*)malloc(sizeof(char) * 100);
-    printf("좋아하는 단어는?");
-    gets(str);
+    // printf("좋아하는 단어는?");
+    // gets(str);
+
+    strcpy(str, "apple");
+    
 
     return str;
 }
